a44cb0cfc05fe9212e937959dffb4a514f091739
Azra1802 <75692990+Azra1802@users.noreply.github.com>
2025-11-12 12:11:55 +0100

    Clean up Program.cs

--- BlueSkyApp/BlueSkyApp/Program.cs
+++ BlueSkyApp/BlueSkyApp/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,11 +19,10 @@ using (var scope = app.Services.CreateScope())
     Console.WriteLine($"Broj korisnika u bazi: {count}");
 }
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
